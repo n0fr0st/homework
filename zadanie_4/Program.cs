db8c72487c9a3b09652df9c47239be3a22dee8c0
n0fr0st <krasnosheckov.v@yandex.ru>
2023-01-20 08:07:22 +0300

    added zadanie 4

---
+++ zadanie_4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число,  программа выпишет все четные числа от 1 до вашего числа.");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= num)
+{
+    if (i%2 == 0)
+    {
+        Console.Write(i + ", ");
+    }
+    i++;
+}
