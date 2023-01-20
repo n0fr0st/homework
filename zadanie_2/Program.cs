c9af42abfd125e8ee67c61effb5ef6fbb978b360
n0fr0st <krasnosheckov.v@yandex.ru>
2023-01-20 07:13:30 +0300

    added zadanie 2 2

--- zadanie_2/Program.cs
+++ zadanie_2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//задание №2
+Console.WriteLine("Введите 3 числа, и программа покажет какое максимальное.");
+Console.WriteLine();
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+int max2;
+if (num2 > num3)
+{
+    max2 = num2;
+}
+else 
+{
+    max2 = num3;
+}
+if (max > max2)
+{
+    Console.WriteLine("Число " + max + " - максимальное.");
+}
+else
+{
+    Console.WriteLine("Число " + max2 + " - максимальное.");
+}
