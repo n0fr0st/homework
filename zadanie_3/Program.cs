95ade0ef800cf24ab7c68070adb2ebeecf5d6480
n0fr0st <krasnosheckov.v@yandex.ru>
2023-01-20 07:33:03 +0300

    added zadanie 3

---
+++ zadanie_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число и программа выяснит, является ли оно четным.");
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+if ((num % 2) == 0)
+{
+    Console.WriteLine("Число " + num + " является четным.");
+}
+else
+{
+    Console.WriteLine("Число " + num + " является нечетным.");
+}
