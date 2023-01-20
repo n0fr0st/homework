57c56fcb23692556454252ce147c019ad4dfa02d
n0fr0st <krasnosheckov.v@yandex.ru>
2023-01-20 06:46:24 +0300

    create new space

--- zadanie_1/Program.cs
+++ zadanie_1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// первое задание
+Console.WriteLine("Введите 2 числа, чтобы вяснить какое большее, а какое меньшее.");
+Console.WriteLine("Введите первое число.");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число.");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("Число " + num1 + " максимальное, а число " + num2 + " минимальное");
+}
+else if (num2 > num1)
+{
+    Console.WriteLine("Число " + num2 + " максимальное, а число " + num1 + " минимальное");
+}
