e13e50796d5e667bc1b9a27ba6d1f0fcfb1a1ee2
n0fr0st <krasnosheckov.v@yandex.ru>
2023-01-24 05:42:20 +0300

    new zadamiya added

--- zadanie_2/Program.cs
+++ zadanie_2/Program.cs
@@ -1,26 +1,14 @@
-﻿//задание №2
-Console.WriteLine("Введите 3 числа, и программа покажет какое максимальное.");
-Console.WriteLine();
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-int num3 = Convert.ToInt32(Console.ReadLine());
-int max = num1;
-int max2;
-if (num2 > num3)
+﻿Console.WriteLine("Введите число и програма покажет 3 по счету цифру.");
+int num = Convert.ToInt32(Console.ReadLine());
+int digit = num;
+if (num > 100)
 {
-    max2 = num2;
+    while (digit > 1000)
+    {
+        digit = digit / 10;
+    }
+    int result = digit % 10;
+    Console.WriteLine(result);
 }
-else 
-{
-    max2 = num3;
-}
-if (max > max2)
-{
-    Console.WriteLine("Число " + max + " - максимальное.");
-}
-else
-{
-    Console.WriteLine("Число " + max2 + " - максимальное.");
-}
+else if (num < 100) 
+Console.WriteLine("Нет третьей цифры.");
--- zadanie_3/Program.cs
+++ zadanie_3/Program.cs
@@ -1,11 +1,29 @@
-﻿Console.WriteLine("Введите число и программа выяснит, является ли оно четным.");
-Console.WriteLine("Введите число ");
-int num = Convert.ToInt32(Console.ReadLine());
-if ((num % 2) == 0)
+﻿Console.WriteLine("Введите цифру и программа выдаст какому дню недели она соответствует и является ли этот день - выходным.");
+int day = Convert.ToInt32(Console.ReadLine());
+switch(day)
 {
-    Console.WriteLine("Число " + num + " является четным.");
-}
-else
-{
-    Console.WriteLine("Число " + num + " является нечетным.");
+    case 1:
+        Console.WriteLine(day + " - Понедельник - не выходной");
+        break;
+    case 2:
+        Console.WriteLine(day + " - Вторник - не выходной");
+        break;
+    case 3:
+        Console.WriteLine(day + " - Среда - не выходной");
+        break;
+    case 4:
+        Console.WriteLine(day + " - Четверг - не выходной");
+        break;
+    case 5:
+        Console.WriteLine(day + " - Пятница - не выходной");
+        break;
+    case 6:
+        Console.WriteLine(day + " - Суббота - выходной");
+        break;
+    case 7:
+        Console.WriteLine(day + " - Воскресенье - выходной");
+        break;
+    default:
+        Console.WriteLine("Ваша цифра не соответствует ни одноу дню недели");
+        break;
 }
