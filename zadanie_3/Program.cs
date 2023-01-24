b41c072f7a41e1a3d55dcd42470c865622eb5ee1
n0fr0st <krasnosheckov.v@yandex.ru>
2023-01-24 05:53:15 +0300

    added new code

--- zadanie_3/Program.cs
+++ zadanie_3/Program.cs
@@ -1,29 +1,12 @@
-﻿Console.WriteLine("Введите цифру и программа выдаст какому дню недели она соответствует и является ли этот день - выходным.");
+﻿Console.WriteLine("Введите цифру и программа выдаст является ли этот день - выходным.");
 int day = Convert.ToInt32(Console.ReadLine());
-switch(day)
+if (day > 0 && day <= 7)
 {
-    case 1:
-        Console.WriteLine(day + " - Понедельник - не выходной");
-        break;
-    case 2:
-        Console.WriteLine(day + " - Вторник - не выходной");
-        break;
-    case 3:
-        Console.WriteLine(day + " - Среда - не выходной");
-        break;
-    case 4:
-        Console.WriteLine(day + " - Четверг - не выходной");
-        break;
-    case 5:
-        Console.WriteLine(day + " - Пятница - не выходной");
-        break;
-    case 6:
-        Console.WriteLine(day + " - Суббота - выходной");
-        break;
-    case 7:
-        Console.WriteLine(day + " - Воскресенье - выходной");
-        break;
-    default:
-        Console.WriteLine("Ваша цифра не соответствует ни одноу дню недели");
-        break;
+    if (day == 6 || day == 7)
+        Console.WriteLine("да");
+    else
+        Console.WriteLine("нет");
 }
+else
+    Console.WriteLine("Ваша цифра не соответствует ни одному дню недели.");
+
